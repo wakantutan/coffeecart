ccc8819eb9b99d2acbfca9fd3021b243324e79c3
wakantutan <aadhor209@gmail.com>
2026-06-16 19:27:11 +0800

    emailsendingfix3

--- coffeecart/settings.py
+++ coffeecart/settings.py
@@ -111,8 +111,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Set EMAIL_HOST_USER and EMAIL_HOST_PASSWORD in Railway environment variables
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
-EMAIL_USE_TLS       = True
+EMAIL_PORT          = 465
+EMAIL_USE_TLS       = False
+EMAIL_USE_SSL       = True
 EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL  = os.environ.get('EMAIL_HOST_USER', '')
